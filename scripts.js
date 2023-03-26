7cd5f85cd2f066eaf2b9a08c935f042fc6d375a4
Julioceferbar <julioceferbar@hotmail.com>
2023-03-26 16:39:54 -0300

    reestruturando com bootstrap02

--- scripts.js
+++ scripts.js
@@ -1,56 +1,53 @@
 // Filtro portfólio
 
-$('.filter-btn').on('click', function() {
+$(".filter-btn").on("click", function () {
+  let type = $(this).attr("id");
+  let boxes = $(".project-box");
 
-    let type = $(this).attr('id');
-    let boxes = $('.project-box');
-  
-    $('.main-btn').removeClass('active');
-    $(this).addClass('active');
-  
-    if(type == 'dsg-btn') {
-      eachBoxes('dsg', boxes);
-    } else if(type == 'dev-btn') {
-      eachBoxes('dev', boxes);
-    } else {
-      eachBoxes('all', boxes);
-    }
-  
-  });
-  
-  function eachBoxes(type, boxes) {
-  
-    if(type == 'all') {
-      $(boxes).fadeIn();
-    } else {
-      $(boxes).each(function() {
-        if(!$(this).hasClass(type)) {
-          $(this).fadeOut('slow');
-        } else {
-          $(this).fadeIn();
-        }
-      });
-    }
+  $(".main-btn").removeClass("active");
+  $(this).addClass("active");
+
+  if (type == "dsg-btn") {
+    eachBoxes("dsg", boxes);
+  } else if (type == "dev-btn") {
+    eachBoxes("dev", boxes);
+  } else {
+    eachBoxes("all", boxes);
   }
+});
 
-  // animação scroll
-const target = document.querySelectorAll('[data-anime]');
-const animationClass = 'animate';
+function eachBoxes(type, boxes) {
+  if (type == "all") {
+    $(boxes).fadeIn();
+  } else {
+    $(boxes).each(function () {
+      if (!$(this).hasClass(type)) {
+        $(this).fadeOut("slow");
+      } else {
+        $(this).fadeIn();
+      }
+    });
+  }
+}
+
+// animação scroll
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
 
 function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach(function(element) {
-        if ((windowTop) > element.offsetTop) {
-            element.classList.add(animationClass);
-        } else {
-            element.classList.remove(animationClass);
-        }
-    })
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  target.forEach(function (element) {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  });
 }
 animeScroll();
 if (target.length) {
-    animeScroll();
+  animeScroll();
 }
-window.addEventListener('scroll', function() {
-    animeScroll();
-})
+window.addEventListener("scroll", function () {
+  animeScroll();
+});
